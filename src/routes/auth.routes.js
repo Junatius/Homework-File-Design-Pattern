da5a8bc7c4d7245bd6fe24e395e567908509b9d8
Junatius <cjuniar0@gmail.com>
2023-10-15 10:52:14 +0800

    feat: add logout endpoint

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,12 @@
 const { Router } = require('express');
 const { authControllers } = require('../controllers');
-const { validator } = require('../middlewares')
+const { validator, authMiddleware } = require('../middlewares')
 
 const router = Router();
 const { requirements, validate } = validator;
 
 router.post('/login', requirements.loginValidator, validate, authControllers.userLogin);
 router.post('/register', requirements.registerValidator, validate, authControllers.registerUser);
+router.post('/logout', authMiddleware.authenticateUser, authControllers.userLogout);
 
 module.exports = router;
-
