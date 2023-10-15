2c961dc85b1c943f6774c47f024d26e64e98a2f4
Junatius <cjuniar0@gmail.com>
2023-10-15 10:53:29 +0800

    feat: add middleware and swagger

--- src/app.js
+++ src/app.js
@@ -2,15 +2,20 @@ const express = require('express');
 const routes = require('./routes');
 const { db } = require('./config');
 const swaggerUi = require('swagger-ui-express');
-const specs = require('./swagger'); 
+const specs = require('./utils/swagger'); 
+const { morganMiddleware } = require('./middlewares');
+const { errorHandler } =require('./middlewares');
 
 const app = express();
 
+app.use(morganMiddleware);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(`/${db.API}`, routes);
+
+app.use(errorHandler);
 
 app.listen(db.PORT, () => {
     console.log(`Server is running on port ${db.PORT}`);
