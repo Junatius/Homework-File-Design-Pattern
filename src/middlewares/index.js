bce57ba2dda62c8e2aeb02dfe766b1bec4f2399b
Junatius <cjuniar0@gmail.com>
2023-10-15 10:51:35 +0800

    feat: import created middleware

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,7 +1,11 @@
 const authMiddleware = require('./auth.middlware');
 const validator = require('./validator');
+const morganMiddleware = require('./morgan.middleware');
+const errorHandler = require('./errorHandler.middlware');
 
 module.exports = {
     authMiddleware,
+    morganMiddleware,
+    errorHandler,
     validator
 }
